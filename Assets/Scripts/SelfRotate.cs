5ff973d40181458d7600436b65c13fe41592fc58
Tu-Ning Ting <tingtntn@gmail.com>
2022-06-14 19:21:37 -0700

    small changes on self rotate script

--- Assets/Scripts/SelfRotate.cs
+++ Assets/Scripts/SelfRotate.cs
@@ -7,17 +7,25 @@ public class SelfRotate : MonoBehaviour
     [SerializeField] float startAngle = 0f;
     [SerializeField] float endAngle = 90f;
     [SerializeField] float duration = 2f;
-    [SerializeField] float selfRotateSpeed = 40f;
+
+
+    [Header("Full Rotate")]
     [SerializeField] bool isFullRotate = false;
-    
-    private void Start() {
+    [SerializeField] float selfRotateSpeed = 40f;
+
+    private void Start()
+    {
 
     }
 
-    private void Update() {
-        if (isFullRotate) {
+    private void Update()
+    {
+        if (isFullRotate)
+        {
             transform.Rotate(new Vector3(0, 0, selfRotateSpeed) * Time.deltaTime);
-        } else {
+        }
+        else
+        {
             float currentAngle = Mathf.SmoothStep(startAngle, endAngle, Mathf.PingPong(Time.time / duration, 1));
             transform.rotation = Quaternion.Euler(0, 0, currentAngle);
         }

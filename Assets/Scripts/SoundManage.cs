c0c674293b8f9033b5ea394349ce183e961e46b2
Hsuan-Chi <hsuanchi@usc.edu>
2022-06-16 19:39:30 -0700

    MainMenu Scene

---
+++ Assets/Scripts/SoundManage.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SoundManage : MonoBehaviour
+{
+    [SerializeField] Slider volumeController;
+    void Awake()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume")) {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+        } 
+
+        Load();
+    }
+
+    
+    public void changeVolume() {
+        AudioListener.volume = volumeController.value;
+        
+    }
+
+    private void Load() {
+        volumeController.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void save() {
+        PlayerPrefs.SetFloat("musicVolume", volumeController.value);
+    }
+}

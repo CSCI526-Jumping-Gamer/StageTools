b549ddfd51952e3b14dc8a70fb25d7ad37b3b28e
Tu-Ning Ting <tingtntn@gmail.com>
2022-06-24 19:17:57 -0700

    fixed small bugs

--- Assets/Scripts/ZeroForceZone.cs
+++ Assets/Scripts/ZeroForceZone.cs
@@ -7,14 +7,24 @@ public class ZeroForceZone : MonoBehaviour
     public bool isTriggered;
     
     private void OnTriggerEnter2D(Collider2D other) {
-        SlingshotHelper slingshotHelper = (SlingshotHelper)Inventory.instance.GetFirstCard();
+        Card card = Inventory.instance.GetFirstCard();
+        SlingshotHelper slingshotHelper = null;
+
+        if (card.GetType() == typeof(SlingshotHelper)) {
+            slingshotHelper = (SlingshotHelper)card;
+        }
 
         if (other.gameObject.tag == "Player" && slingshotHelper && slingshotHelper.isHelperEnabled) {
             isTriggered = true;
         }
     }
     private void OnTriggerExit2D(Collider2D other) {
-        SlingshotHelper slingshotHelper = (SlingshotHelper)Inventory.instance.GetFirstCard();
+        Card card = Inventory.instance.GetFirstCard();
+        SlingshotHelper slingshotHelper = null;
+
+        if (card.GetType() == typeof(SlingshotHelper)) {
+            slingshotHelper = (SlingshotHelper)card;
+        }
 
         if (other.gameObject.tag == "Player" && slingshotHelper && slingshotHelper.isHelperEnabled) {
             isTriggered = false;

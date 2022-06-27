ca619b573dc1ef3d92dce09b49f7955540a0ddff
Tu-Ning Ting <tingtntn@gmail.com>
2022-06-27 14:18:34 -0700

    added DeathDetector analyticis

--- Assets/Scripts/DeathDetector.cs
+++ Assets/Scripts/DeathDetector.cs
@@ -3,32 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class DeathDetector : MonoBehaviour
+namespace Unity.Services.Analytics
 {
-    [SerializeField] float loadDelay = 0.5f;
-    // [SerializeField] ParticleSystem crashEffect;
-    PlayerController playerController;
-    // [SerializeField] AudioClip crashSFX;
-    // Start is called before the first frame update
-    [SerializeField] bool isLevel2;
-    Teleporter teleporter;
-    private void Awake() {
-        playerController = FindObjectOfType<PlayerController>();
-        teleporter = FindObjectOfType<Teleporter>();
-    }
-    void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Player") {
-            // crashEffect.Play();
-            // GetComponent<AudioSource>().PlayOneShot(crashSFX); 
-            Invoke("ReloadScene", loadDelay);
+    public class DeathDetector : MonoBehaviour
+    {
+        [SerializeField] float loadDelay = 0.5f;
+        // [SerializeField] ParticleSystem crashEffect;
+        PlayerController playerController;
+        // [SerializeField] AudioClip crashSFX;
+
+        [SerializeField] bool isLevel2;
+        Teleporter teleporter;
+        private void Awake() {
+            playerController = FindObjectOfType<PlayerController>();
+            teleporter = FindObjectOfType<Teleporter>();
         }
-    }
-    void ReloadScene() {
-        if (!isLevel2) {
-            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(currentSceneIndex);
-        } else {
-            teleporter.Respawning();
+        void OnTriggerEnter2D(Collider2D other) {
+            if (other.tag == "Player") {
+                // crashEffect.Play();
+                // GetComponent<AudioSource>().PlayOneShot(crashSFX);
+                Debug.Log("player died");
+                AnalyticsService.Instance.CustomData("playerDied", new Dictionary<string, object>());
+                Invoke("ReloadScene", loadDelay);
+            }
+        }
+        void ReloadScene() {
+            if (!isLevel2) {
+                int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(currentSceneIndex);
+            } else {
+                teleporter.Respawning();
+            }
         }
     }
 }

32c4fa35fb7e14990bfc7fd7cb184c4f32c15c2a
Zhen Qin <zhenq@usc.edu>
2022-06-29 15:32:28 -0700

    Update level 7 and Magnet Field Layer

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -2,24 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class Laser : MonoBehaviour
 {
+    CardTimer cardTimer;
+    public PlayerController playerController;
     [SerializeField] private float defDistanceRay = 50;
+    [SerializeField] float loadDelay = 0.2f;
+    DeltaDnaEventHandler deltaDnaEventHandler;
+
     public Transform laserFirePoint;
     public LineRenderer m_lineRenderer;
- //   public GameManager gameManager;
- //   public Shield shield;
+    //   public GameManager gameManager;
+    //   public Shield shield;
     private float count; // Invincible time
     // private float invisible_time;
     private bool flag;
     Transform m_transform;
+    RaycastHit2D _hit;
+    Rigidbody2D otherRb2d;
+
 
     private void Awake()
     {
+        cardTimer = FindObjectOfType<CardTimer>();
+        deltaDnaEventHandler = FindObjectOfType<DeltaDnaEventHandler>();
+
+        playerController = FindObjectOfType<PlayerController>();
         count = 0;
         // invisible_time = 1;
         flag = false;
         m_transform = GetComponent<Transform>();
+
     }
 
     public void Update()
@@ -33,38 +47,45 @@ public class Laser : MonoBehaviour
 
     void ShootLaser()
     {
-        if (Physics2D.Raycast(m_transform.position, transform.right) )
+        if (Physics2D.Raycast(m_transform.position, transform.right))
         {
-            RaycastHit2D _hit = Physics2D.Raycast(laserFirePoint.position, transform.right);
-            Debug.Log(_hit.transform.tag.ToString());
+            _hit = Physics2D.Raycast(laserFirePoint.position, transform.right);
             if (_hit.transform.tag.ToString() == "Player")
             {
-                Invoke("ReloadScene", 0);
+                otherRb2d = _hit.collider.gameObject.GetComponent<Rigidbody2D>();
+
+                if (PlayerController.instance.shieldCount > 0)
+                {
+                    // analyticsEventHandler.RecordShieldUsed(other.transform.position);
+                    PlayerController.instance.shieldCount -= 1;
+                    if (PlayerController.instance.shieldCount == 0)
+                    {
+                        cardTimer.Deactivate();
+                    }
+                }
+                else
+                {
+                    // analyticsEventHandler.RecordPlayerDeath(other.transform.position);
+                    // gameAnalyticsEventHandler.RecordPlayerDied(other.transform.position);
+                    playerController.DisablePlayerInput();
+                    string propName = gameObject.name;
+                    int propID = gameObject.GetInstanceID();
+                    // Debug.Log(propName + '/' + propID);
+                    // deltaDnaEventHandler.RecordPlayerDied(otherRb2d.transform.position, propName, propID);
+                    Invoke("Respawning", loadDelay);
+                }
+
             }
-            Physics2D.IgnoreLayerCollision(5,2,true);
-        /*    if (_hit.transform.tag.ToString() == "Magnet")
-            {
-                
-                float x = laserFirePoint.transform.right.x;
-                float y = laserFirePoint.transform.right.y;
-                _hit = Physics2D.Raycast(laserFirePoint.position, transform.right);
-                Vector2 endPos = new Vector2(laserFirePoint.position.x + x * defDistanceRay, laserFirePoint.position.y + y * defDistanceRay);
-                Draw2DRay(laserFirePoint.position, _hit.point);
-            }*/
-         //   else
-           // {
-             Draw2DRay(laserFirePoint.position, _hit.point);
-         //   }
+            // Physics2D.IgnoreLayerCollision(5,2,true);
+            Draw2DRay(laserFirePoint.position, _hit.point);
         }
         else
         {
-            //      Draw2DRay(laserFirePoint.position, laserFirePoint.transform.right * defDistanceRay);
             float x = laserFirePoint.transform.right.x;
             float y = laserFirePoint.transform.right.y;
 
             Vector2 endPos = new Vector2(laserFirePoint.position.x + x * defDistanceRay, laserFirePoint.position.y + y * defDistanceRay);
             Draw2DRay(laserFirePoint.position, endPos);
-
         }
     }
 
@@ -74,9 +95,16 @@ public class Laser : MonoBehaviour
         m_lineRenderer.SetPosition(1, endPos);
     }
 
-    void ReloadScene()
+    // void ReloadScene()
+    // {
+    //     int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    //     SceneManager.LoadScene(currentSceneIndex);
+    // }
+
+    void Respawning()
     {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex);
+        // Debug.Log(playerController);
+        otherRb2d.transform.position = playerController.GetCheckPointPosition();
+        playerController.EnablePlayerInput();
     }
 }

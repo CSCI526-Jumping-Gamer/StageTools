00929d217b4d61316a6458701415dcdb92aaf9bf
Zhen Qin <zhenq@usc.edu>
2022-06-29 22:29:40 -0700

    Fix Bugs in Level 5 and 7; Revert ObjectDetector to its previous version

--- Assets/Scripts/ObjectDetector.cs
+++ Assets/Scripts/ObjectDetector.cs
@@ -4,23 +4,14 @@ using UnityEngine;
 
 public class ObjectDetector : MonoBehaviour
 {
-    PlayerController playerController;
-    // Start is called before the first frame update
-    private void Awake()
-    {
-        playerController = FindObjectOfType<PlayerController>();
-    }
-
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") {
-            playerController.SetIsChildOfMagnet(true);
             other.transform.parent = transform;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
         if (other.tag == "Player") {
-            playerController.SetIsChildOfMagnet(false);
             other.transform.parent = null;
 			other.transform.localEulerAngles = new Vector3(0,0,0);
         }

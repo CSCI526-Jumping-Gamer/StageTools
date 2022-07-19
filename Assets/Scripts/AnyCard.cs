f680c8f89704e8a2e088188a6580bbd8a7565ffd
Eden_Ye <edenye00@gmail.com>
2022-07-19 01:27:52 -0700

    Add card pop-up feature

--- Assets/Scripts/AnyCard.cs
+++ Assets/Scripts/AnyCard.cs
@@ -6,10 +6,11 @@ public class AnyCard : MonoBehaviour
 {
     [SerializeField] Card card;
     bool CardTriggered;
+    [SerializeField] CardPopUp cardPopUp;
 
     void Start()
     {
-
+        cardPopUp = FindObjectOfType<CardPopUp>();
     }
 
     // Update is called once per frame
@@ -21,6 +22,14 @@ public class AnyCard : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player" && !CardTriggered && Inventory.instance.cards.Count <= 1) {
+            if (!Inventory.instance.cards.ContainsKey(0))
+            {
+                cardPopUp.CreateCardPopUp(card, 0);
+            }
+            else if (Inventory.instance.cards.ContainsKey(0) && !Inventory.instance.cards.ContainsKey(1))
+            {
+                cardPopUp.CreateCardPopUp(card, 1);
+            }
             Inventory.instance.Add(card);
             CardTriggered = true;
             Invoke("DestroyWithLatency", 0.2f);

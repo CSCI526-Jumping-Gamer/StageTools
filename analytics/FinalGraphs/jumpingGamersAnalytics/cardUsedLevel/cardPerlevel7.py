f4793a3593dceb01c10673e84f039fe71301904e
Utkarsh Bhardwaj <bhardwajutkarsh07@gmail.com>
2022-07-20 12:17:44 -0700

    graphs

--- analytics/FinalGraphs/jumpingGamersAnalytics/cardUsedLevel/cardPerlevel7.py
+++ analytics/FinalGraphs/jumpingGamersAnalytics/cardUsedLevel/cardPerlevel7.py
@@ -3,16 +3,16 @@ import pandas as pd
 
 # Card used graph
 
-df = pd.read_csv("/Users/utkarshbhardwaj/Documents/Lectures/mobileDevices/analytics/new.csv", usecols = ['CARDNAME','SCENENAME'])
+df = pd.read_csv("/Users/utkarshbhardwaj/Documents/Lectures/mobileDevices/analytics/new.csv", usecols = ['CARDNAME','SCENENAME','EVENTNAME'])
 df['CARDNAME'] = df['CARDNAME'].fillna("NA")
 df['SCENENAME'] = df['SCENENAME'].fillna("NA")
 
 result = {}
 total_attempts = 0
 for ind in df.index:
-    if df['SCENENAME'][ind] == "NA" or df['CARDNAME'][ind] == "NA" :
+    if df['SCENENAME'][ind] == "NA" or df['EVENTNAME'][ind] not in ['playerDied', 'levelCompleted'] :
         continue
-    if 'Level 7' in df['SCENENAME'][ind]:
+    if 'Level 7' in df['SCENENAME'][ind] and df['EVENTNAME'][ind] in ['playerDied', 'levelCompleted']:
         total_attempts +=1
     else:
         continue
@@ -43,9 +43,13 @@ print(result)
 
 y = list(result.values())
 print(y)
-
+total = sum(y)
+def absolute_value(val):
+    a  = (val/total_attempts) *100
+    return a
 mylabels = result.keys()
 print(mylabels)
-plt.pie(y, labels = mylabels)
-plt.title('Card Used at level 7', fontsize=14)
+plt.pie(y, labels = mylabels, autopct=lambda p: '{:.0f}'.format(p * total / 100),startangle=90)
+s = 'Card Used at level 7(total_Attempts = ' + str(total_attempts) + ')'
+plt.title(s, fontsize=14)
 plt.show()
